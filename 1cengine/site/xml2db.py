dbe2d69e76ab9e3e99802e2a5c1bd5a6f410db61
alexander <elf607@ya.ru>
2012-09-20 11:53:21 +0600

    conflict - delete file in future

---
+++ 1cengine/site/xml2db.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import simplejson as json
+from xml import sax
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+pricefile = open('price.xml', 'r')
+xml_string = pricefile.read()
+pricefile.close()
+# print xml_string
+
+xmlList = []
+
+def display(data):
+    # import pprint
+    # pp = pprint.PrettyPrinter(depth=10)
+    # pp.pprint(data)
+
+    # testfile = open('testfile', 'w')
+    # testfile.write(data)
+
+    xmlList.append(data) 
+
+    # print data[0][u'Характеристика']
+
+    # for x in data[u'Предмет']:
+    #     print x[u'Характеристика']
+
+class Element:
+    def setData(self, key, value):
+        self.__dict__[key] = value
+
+    def setObject(self, key, object):
+        if key in self.__dict__ and not isinstance(self.__dict__[key], (list, tuple)):
+            prev_object = self.__dict__[key]
+            self.__dict__[key] = []
+            self.__dict__[key].append(prev_object)
+            self.__dict__[key].append(object)
+        elif key in self.__dict__:
+            self.__dict__[key].append(object)
+        else:
+            self.__dict__[key] = object
+
+    def jsonable(self):
+        return self._traverse(self.__dict__)
+
+    # http://stackoverflow.com/questions/1036409/recursively-convert-python-object-graph-to-dictionary/1118038#1118038
+    def _traverse(self, obj):
+        if isinstance(obj, dict):
+            for k in obj.keys():
+                obj[k] = self._traverse(obj[k])
+            return obj
+        elif hasattr(obj, "__iter__"):
+            return [self._traverse(v) for v in obj]
+        elif hasattr(obj, "__dict__"):
+            data = dict([(key, self._traverse(value))
+                for key, value in obj.__dict__.iteritems()
+                if not callable(value) and not key.startswith('_')])
+            return data
+        else:
+            return obj
+
+class ObjBuilder(sax.ContentHandler):
+    def __init__(self, node):
+        sax.ContentHandler.__init__(self)
+        self.obj = []
+        self.node = node
+        self.fetch = False
+        self.__buffer = ''
+
+    def startElementNS(self, name, qname, attrs):
+        (ns, localname) = name
+        if self.node == localname:
+            self.fetch = True
+            o = Element()
+            self.rootobject = o
+            self.obj.append(o)
+        elif self.fetch:
+            self.__buffer = ''
+            o = Element()
+            self.obj[-1].setObject(localname, o)
+            self.obj.append(o)
+
+    def characters(self,contents):
+        if self.fetch:
+            self.__buffer += contents.strip()
+
+    def endElementNS(self, name, qname):
+        (ns, localname) = name
+        if self.node == localname:
+            self.fetch = False
+            display(self.rootobject.jsonable())
+            data = self.rootobject.jsonable()
+        elif self.fetch:
+            if self.__buffer != '':
+                self.obj[-2].setData(localname, self.__buffer)
+            del self.obj[-1]
+            self.__buffer = ''
+
+def insertQuery(iName, pHash, cName, weight, length, kf, iHash, edIzm, price, priceType, groupSecondName, itemHashN, inStock):
+    import MySQLdb
+
+    conn = MySQLdb.connect (host = "localhost",
+                           user = "trimetru_goods",
+                           passwd = "&rUI24*(^o",
+                           db = "trimetru_goods")
+    cursor = conn.cursor()
+    # query = "INSERT INTO `trimetru_goods`.`offers` (`id`, `name`, `hash`, `parent_hash`, `display_name`, `char_name`, `weight`, `length`, `kf`, `edIzm`, `price`, `price_type`, `father_hash`, `stock`) VALUES ('null','"+mysql_escape_string(groupSecondName)+" "+mysql_escape_string(cName)+" ','"+iHash+"','"+pHash+"','"+mysql_escape_string(iName)+"','"+mysql_escape_string(cName)+"','"+weight+"','"+length+"','"+kf+"','"+edIzm+"','"+mysql_escape_string(price)+"','"+mysql_escape_string(priceType)+"','"+itemHashN+"', '"+inStock+"');"
+    
+    cursor.execute (""" INSERT INTO `trimetru_goods`.`offers` (`id`, `name`, `hash`, `parent_hash`, `display_name`, `char_name`, `weight`, `length`, `kf`, `edIzm`, `price`, `price_type`, `father_hash`, `stock`) VALUES ( %s ) """, ('null',iName, pHash, cName, weight, length, kf, iHash, edIzm, price, priceType, groupSecondName, itemHashN, inStock))
+    row = cursor.fetchone ()
+    print row[0]
+    cursor.close ()
+    conn.close ()
+
+
+def groupEater(group):
+    # print '\/\/\/\/\/\/\/\/\/\/\/\/'
+    
+    if u'Предмет' in group:
+        # print group[u'НаименованиеГруппы']
+        # print group[u'НоменклатураСсылка']
+        # print group[u'Синоним']
+        if type(group[u'Предмет'])==type(list()):
+            for itemChar in group[u'Предмет']:
+            
+                # print itemChar[u'Характеристика']
+                # print itemChar[u'Вес']
+                # print itemChar[u'Кратность']
+                # print itemChar[u'Коэффициент']
+                # print itemChar[u'ХарактеристикаСсылка']
+                # print itemChar[u'ЕдИзмерения']
+                priceDB = []
+                priceType = []
+                for price in itemChar[u'Цена']:
+                    priceDB.append(price[u'Цена'])
+                    priceType.append(price[u'НазваниеЦены'])
+                    # print price[u'НазваниеЦены'] +' '+ price[u'Цена']
+                # print itemChar[u'ЕстьВНаличии']
+
+                # print '||'+group[u'НаименованиеГруппы']+', '+pHash+', '+itemChar[u'Характеристика']+', '+itemChar[u'Вес']+', '+itemChar[u'Кратность']+', \
+            # '+itemChar[u'Коэффициент']+', '+itemChar[u'ХарактеристикаСсылка']+', '+itemChar[u'ЕдИзмерения']+', '+"|".join(priceDB)+', '+"|".join(priceType)+', \
+            # '+group[u'Синоним']+', '+group[u'НоменклатураСсылка']+', '+itemChar[u'ЕстьВНаличии']
+
+                insertQuery(group[u'НаименованиеГруппы'], pHash, itemChar[u'Характеристика'], itemChar[u'Вес'], itemChar[u'Кратность'], itemChar[u'Коэффициент'], itemChar[u'ХарактеристикаСсылка'], itemChar[u'ЕдИзмерения'], "|".join(priceDB), "|".join(priceType), group[u'Синоним'], group[u'НоменклатураСсылка'], itemChar[u'ЕстьВНаличии'])
+
+                print '-----------------------------------------------'
+        elif type(group[u'Предмет']==type(dict)):
+            # print group[u'Предмет'][u'Характеристика']
+            # print group[u'Предмет'][u'Вес']
+            # print group[u'Предмет'][u'Кратность']
+            # print group[u'Предмет'][u'ХарактеристикаСсылка']
+            # print group[u'Предмет'][u'ЕдИзмерения']
+            priceDB = []
+            priceType = []
+            for price in group[u'Предмет'][u'Цена']:
+                priceDB.append(price[u'Цена'])
+                priceType.append(price[u'НазваниеЦены'])
+            #     print price[u'НазваниеЦены'] +' '+ price[u'Цена']
+            # print group[u'Предмет'][u'ЕстьВНаличии']
+            # print '||'+group[u'НаименованиеГруппы']+', '+pHash+', '+group[u'Предмет'][u'Характеристика']+', '+group[u'Предмет'][u'Вес']+', '+group[u'Предмет'][u'Кратность']+', \
+            # '+group[u'Предмет'][u'Коэффициент']+', '+group[u'Предмет'][u'ХарактеристикаСсылка']+', '+group[u'Предмет'][u'ЕдИзмерения']+', '+"|".join(priceDB)+', '+"|".join(priceType)+', \
+            # '+group[u'Синоним']+', '+group[u'НоменклатураСсылка']+', '+group[u'Предмет'][u'ЕстьВНаличии']
+
+            insertQuery(group[u'НаименованиеГруппы'], pHash, group[u'Предмет'][u'Характеристика'], group[u'Предмет'][u'Вес'], group[u'Предмет'][u'Кратность'], group[u'Предмет'][u'Коэффициент'], group[u'Предмет'][u'ХарактеристикаСсылка'], group[u'Предмет'][u'ЕдИзмерения'], "|".join(priceDB), "|".join(priceType), group[u'Синоним'], group[u'НоменклатураСсылка'], group[u'Предмет'][u'ЕстьВНаличии'])
+
+            print '-----------------------------------------------'
+
+    if u'Цена' in group:
+        print group[u'ЕдИзмерения']
+        if type(group[u'Цена'])==type(list()):
+            for price in group[u'Цена']:
+                    print price[u'НазваниеЦены'] +' '+ price[u'Цена']
+        elif type(group[u'Цена'])==type(dict()):
+            print group[u'Цена'][u'НазваниеЦены'] +' '+ group[u'Цена'][u'Цена']
+            print pHash
+        print '-----------------------------------------------'
+
+
+
+    if u'Группа' in group:
+        print 1
+        # try:
+        print group[u'НаименованиеГруппы']
+        # except:
+        #     print group[u'Группа']
+            # print group
+        print group[u'НоменклатураСсылка']
+        if type(group[u'Группа'])==type(list()):
+            for subgroup in group[u'Группа']:
+                groupEater(subgroup)
+        elif type(group[u'Группа'])==type(dict()):
+            groupEater(group[u'Группа'])
+
+if __name__ == '__main__':
+    parser = sax.make_parser()
+    parser.setContentHandler(ObjBuilder('СписокНоменклатуры'.decode("utf-8")))
+    parser.setFeature(sax.handler.feature_namespaces, 1)
+
+    inpsrc = sax.xmlreader.InputSource()
+    inpsrc.setByteStream(StringIO(xml_string))
+    parser.parse(inpsrc)
+
+
+    for group in xmlList[0][u'Группа']:
+        print group[u'НаименованиеГруппы']
+        print group[u'НоменклатураСсылка']
+
+        pHash = group[u'НоменклатураСсылка']
+
+        if u'Группа' in group:
+            if type(group[u'Группа'])==type(dict()):
+                groupEater(group[u'Группа'])
+            elif type(group[u'Группа'])==type(list()):
+                for subgroup in group[u'Группа']:
+                    groupEater(subgroup)
+
+        print '######################################'
+
+
+    
