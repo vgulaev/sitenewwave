375b4e38ef3b75d8634bcc3d3254f3dc9dd4328b
vgulaev <vgulaev@yandex.ru>
2012-12-14 14:46:02 +0600

    Flying button

--- myjsscripts.js
+++ myjsscripts.js
@@ -6,16 +6,26 @@ function selector_constructor(idElement, welcomLabel, items) {
 	header = document.createElement("div");
 	header.id = idElement + "Header";
 	header.innerHTML = welcomLabel;
-	$("#"+idElement).append(header);
-	
+
+	$("#" + idElement).append(header);
+
 }
 
 function showalert() {
 	alert("Hello!!!");
 }
 
+function example() {
+	/*$("#metalSelector").animate({
+	 "height": "toggle", "opacity": "toggle"
+	 }, "slow");*/
+	$("#metalSelector").css("position", "absolute");
+	$("#metalSelector").animate({"left": "+=50px"}, "slow");
+
+}
+
 
 $(document).ready(function() {
 	//alert("Hello!!!");
-	selector_constructor("profilName", "Выберите профиль...",["Арматура", "Швелер", "Провола", "Балка"]);
+	selector_constructor("profilName", "Выберите профиль...", ["Арматура", "Швелер", "Провола", "Балка"]);
 })
