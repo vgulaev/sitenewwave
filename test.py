de2a1738d33cb43b28c24025576966d1db7fc506
vgulaev <vgulaev@yandex.ru>
2012-12-17 20:15:19 +0600

    Prepeare for create Header

--- test.py
+++ test.py
@@ -5,33 +5,37 @@ import cgitb; cgitb.enable()
 #sys.path.insert(0, os.path.expanduser('~/site/python'))
 from xml.dom.minidom import DOMImplementation, getDOMImplementation
 
-print ("Content-Type: text/html")
-print ("")
-#print ("Hello!!!")
-#print (sys.version)
-#x = htmltag()
-#print (x.tagName)
+if ((sys.platform) == "win32"):
+    #print ("")
+    sys.stdout = open('temp.html', 'w')
+else:
+    print ("Content-Type: text/html")
+    print ("")
 
 impl = getDOMImplementation()
 
 html_doc = impl.createDocument(None, "html", None)
 
-child = html_doc.createElement("head")
-html_doc.documentElement.appendChild(child)
+childHead = html_doc.createElement("head")
+
+chil = html_doc.createElement("link")
+chil.setAttribute("rel", "stylesheet")
+chil.setAttribute("href", "mycss.css")
+
+childHead.appendChild(chil)
+
+html_doc.documentElement.appendChild(childHead)
 
 child = html_doc.createElement("body") 
 html_doc.documentElement.appendChild(child)
 
-childh1 = html_doc.createElement("h1")
-childh1.setAttribute("id", "idh1") 
+childh1 = html_doc.createElement("div")
+childh1.setAttribute("id", "divmain")
+childh1.setAttribute("class", "for_debug_div_blockmodel") 
 
-child = html_doc.createTextNode("Hello word!!!")
+child = html_doc.createTextNode("Hello!!!")
 childh1.appendChild(child)
 
 html_doc.documentElement.appendChild(childh1)
 
-#html_doc.appendChild(child)
 print (html_doc.toxml())
-
-#print ("6ef8395fecf207165f1a82178ae1b984".__len__())
-#f02901f3e39e2e6131c4bf00db7aa726
