641549972aa6af0bd21e61db514dc4dd3666eb64
vgulaev <vgulaev@yandex.ru>
2012-12-18 12:56:09 +0600

    Build template

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
-#!/web/tdymkru/python/bin/python
+#!/usr/bin/python2.6
+# -*- coding: utf-8 -*-
 # This Python file uses the following encoding: utf-8
 import sys,os
 import cgi
@@ -7,12 +8,15 @@ from htmlrootclass import htmlroot
 
 if ((sys.platform) == "win32"):
     print ("")
-    sys.stdout = open('temp.html', 'w')
+    #sys.stdout = open('temp.html', 'w')
 else:
     print ("Content-Type: text/html; charset=utf-8")
     print ("")
 
-result_doc = htmlroot()
-result_doc.initHTML()
+#result_doc = htmlroot()
+#result_doc.initHTML()
 
-print (result_doc.html_doc.toxml())
+#print (result_doc.html_doc.toxml())
+
+f = open("mainpage_template.html", "r")
+print (f.read())
--- htmlrootclass.py
+++ htmlrootclass.py
@@ -1,26 +1,35 @@
-#!/web/tdymkru/python/bin/python
+#!/usr/bin/python2.6
 # This Python file uses the following encoding: utf-8
 from xml.dom.minidom import DOMImplementation, getDOMImplementation
 
 class htmlroot:
     def initHTML(self):
         impl = getDOMImplementation()
+        #root element
         self.html_doc = impl.createDocument(None, "html", None)
-        childHead = self.html_doc.createElement("head")
+        #start head element
+        htmlHead = self.html_doc.createElement("head")
         chil = self.html_doc.createElement("meta")
         chil.setAttribute("content", "text/html; charset=utf-8")
         chil.setAttribute("http-equiv", "content-type")
-        childHead.appendChild(chil)
+        htmlHead.appendChild(chil)
         chil = self.html_doc.createElement("link")
         chil.setAttribute("rel", "stylesheet")
         chil.setAttribute("href", "mycss.css")
-        childHead.appendChild(chil)
-        self.html_doc.documentElement.appendChild(childHead)
-        child = self.html_doc.createElement("body") 
-        self.html_doc.documentElement.appendChild(child)
-        childh1 = self.html_doc.createElement("div")
-        childh1.setAttribute("id", "divmain")
-        childh1.setAttribute("class", "for_debug_div_blockmodel")
-        child = self.html_doc.createTextNode("Привет!!!")
-        childh1.appendChild(child)
-        self.html_doc.documentElement.appendChild(childh1)
+        htmlHead.appendChild(chil)
+        self.html_doc.documentElement.appendChild(htmlHead)
+        #end head element
+        htmlBody = self.html_doc.createElement("body") 
+        #self.html_doc.documentElement.appendChild(child)
+        divMain = self.html_doc.createElement("div")
+        divMain.setAttribute("id", "divmain")
+        divHead = self.html_doc.createElement("div")
+        divHead.setAttribute("id", "divhead")
+        divHead.setAttribute("class", "for_debug_div_blockmodel")
+        #chil = self.html_doc.createElement("img")
+        #chil = 
+        #divMain.appendChild(chil)
+        divMain.appendChild(divHead)
+        htmlBody.appendChild(divMain)
+        self.html_doc.documentElement.appendChild(htmlBody)
+        
