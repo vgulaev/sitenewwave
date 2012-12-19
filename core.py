d0d74180c8e741817f79d6a8ce179945f4ce08a0
vgulaev <vgulaev@yandex.ru>
2012-12-19 17:16:44 +0600

    Commit for commit

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/web/tdymkru/python/bin/python2.6
 # -*- coding: utf-8 -*-
 # This Python file uses the following encoding: utf-8
 import sys,os
@@ -7,7 +7,9 @@ import cgitb; cgitb.enable()
 from htmlrootclass import htmlroot
 from xml.dom.minidom import DOMImplementation, getDOMImplementation
 from xml.dom.minidom import parse, parseString
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
+sys.path.insert(0, os.path.expanduser('~/site/python'))
+#import lxml 
 
 if ((sys.platform) == "win32"):
     print ("")
@@ -22,6 +24,10 @@ else:
 #print (result_doc.html_doc.toxml())
 
 #f = open("htmlstaticcontent/002aboutcompany/index.html", "r")
+domHeader = parse("mainpage_template.html")
+headReceiver = domHeader.getElementsByTagName('head')[0];
+bodyReceiver = domHeader.getElementsByTagName('body')[0];
+
 dom1 = parse("htmlstaticcontent/002aboutcompany/index.html")
 
 name = dom1.getElementsByTagName('link')
@@ -32,9 +38,16 @@ name = dom1.getElementsByTagName('img')
 for x in name:
      x.setAttribute("src", "htmlstaticcontent/002aboutcompany/" + x.getAttribute("src"))
 
-print(dom1.toxml())
-# dom2 = ET.parse("mainpage_template.html")
+#print(dom1.toxml())
 
-# ET.register_namespace("", "")
+bodyForImport = dom1.getElementsByTagName('body')[0]
 
-# print(ET.tostring(dom2.getroot(), "utf-8", "xml"))
+for x in bodyForImport.childNodes:
+    bodyReceiver.appendChild(x)
+
+#import all styles    
+name = dom1.getElementsByTagName("link")
+for x in name:
+    headReceiver.appendChild(x)
+    
+print domHeader.toxml("utf-8")
