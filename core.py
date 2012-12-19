a44eb51d9505f5241829a15a4c7287260b254168
vgulaev <vgulaev@yandex.ru>
2012-12-19 13:32:17 +0600

    Corect main part of main page

--- core.py
+++ core.py
@@ -4,7 +4,9 @@
 import sys,os
 import cgi
 import cgitb; cgitb.enable()
-from htmlrootclass import htmlroot 
+from htmlrootclass import htmlroot
+from xml.dom.minidom import DOMImplementation, getDOMImplementation
+from xml.dom.minidom import parse, parseString 
 
 if ((sys.platform) == "win32"):
     print ("")
@@ -18,8 +20,19 @@ else:
 
 #print (result_doc.html_doc.toxml())
 
-#f = open("mainpage_template.html", "r")
-#print (f.read())
-form = cgi.FieldStorage()
+#f = open("htmlstaticcontent/0001mainpage/index.html", "r")
+dom1 = parse("htmlstaticcontent\\0001mainpage\\index.xml")
 
-print "<p>name:", form["name"].value
+name = dom1.getElementsByTagName('link')
+for x in name:
+    x.setAttribute("href", "htmlstaticcontent/0001mainpage/" + x.getAttribute("href"))
+
+name = dom1.getElementsByTagName('img')
+for x in name:
+    x.setAttribute("src", "htmlstaticcontent/0001mainpage/" + x.getAttribute("src"))
+
+print(dom1.toxml())
+#print (f.read())
+#form = cgi.FieldStorage()
+
+#print "<p>name:", form["name"].value
