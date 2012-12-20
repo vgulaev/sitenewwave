0fbc850acab6981fe6baceaa9e6616cf0b328d9f
vgulaev <vgulaev@yandex.ru>
2012-12-20 14:12:03 +0600

    commit for commit

--- core.py
+++ core.py
@@ -4,46 +4,23 @@
 import sys,os
 import cgi
 import cgitb; cgitb.enable()
-from htmlrootclass import htmlroot
 from xml.dom.minidom import DOMImplementation, getDOMImplementation
 from xml.dom.minidom import parse, parseString
-import xml.etree.ElementTree as ET
-import html5lib
 
 if ((sys.platform) == "win32"):
     print ("")
-    sys.stdout = open('temp.html', 'w')
+    #sys.stdout = open('temp.html', 'w')
 else:
     print ("Content-Type: text/html; charset=utf-8")
     print ("")
 
-f = open("mainpage_template.html")
-doc = html5lib.parse(f)
-print(doc.toxml())
-#domHeader = parse("mainpage_template.html")
-# headReceiver = domHeader.getElementsByTagName('head')[0];
-# bodyReceiver = domHeader.getElementsByTagName('body')[0];
+domHeader = parse("mainpage_template.html")
 
-# dom1 = parse("htmlstaticcontent/002aboutcompany/index.html")
+nodes = domHeader.getElementsByTagName('div')
+for element in nodes:
+    if element.hasAttribute("id"):
+         element.setIdAttribute("id")
+         
+elem = domHeader.getElementById("main")
 
-# name = dom1.getElementsByTagName('link')
-# for x in name:
-    # x.setAttribute("href", "htmlstaticcontent/002aboutcompany/" + x.getAttribute("href"))
-
-# name = dom1.getElementsByTagName('img')
-# for x in name:
-     # x.setAttribute("src", "htmlstaticcontent/002aboutcompany/" + x.getAttribute("src"))
-
-# bodyForImport = dom1.getElementsByTagName('body')[0]
-
-# for x in bodyForImport.childNodes:
-    # bodyReceiver.appendChild(x)
-
-# name = dom1.getElementsByTagName("link")
-# for x in name:
-    # headReceiver.appendChild(x)
-    
-#print domHeader.toxml("utf-8")
-
-#tree = ET.parse('mainpage_template.html')
-#print(ET.tostring(tree.getroot(), "utf-8"))
+print(domHeader.toxml("utf-8"))
