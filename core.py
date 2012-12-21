488878698a13f023417bbb83e3ed783224988d89
vgulaev <vgulaev@yandex.ru>
2012-12-21 16:55:43 +0600

    working code with BeautifulSoup

--- core.py
+++ core.py
@@ -6,21 +6,22 @@ import cgi
 import cgitb; cgitb.enable()
 from xml.dom.minidom import DOMImplementation, getDOMImplementation
 from xml.dom.minidom import parse, parseString
+from xml.etree.ElementTree import tostring
+from bs4 import BeautifulSoup
 
 if ((sys.platform) == "win32"):
     print ("")
-    #sys.stdout = open('temp.html', 'w')
+    sys.stdout = open('temp.html', 'w')
 else:
     print ("Content-Type: text/html; charset=utf-8")
     print ("")
+    
+print("<!DOCTYPE html>")
 
-domHeader = parse("mainpage_template.html")
+soup = BeautifulSoup(open("mainpage_template.html"))
 
-nodes = domHeader.getElementsByTagName('div')
-for element in nodes:
-    if element.hasAttribute("id"):
-         element.setIdAttribute("id")
-         
-elem = domHeader.getElementById("main")
+soupForImport = BeautifulSoup(open("htmlstaticcontent/0001mainpage/index.html"))
+nodes = soupForImport.find(id="main")
+soup.html.body.append(nodes)
 
-print(domHeader.toxml("utf-8"))
+print(soup.prettify())
