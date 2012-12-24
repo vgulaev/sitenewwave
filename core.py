ee7ef216bcbf193b2d711d96e62e9c9432f4b07f
vgulaev <vgulaev@yandex.ru>
2012-12-24 13:27:14 +0600

    Commit for commit

--- core.py
+++ core.py
@@ -4,9 +4,6 @@
 import sys,os
 import cgi
 import cgitb; cgitb.enable()
-from xml.dom.minidom import DOMImplementation, getDOMImplementation
-from xml.dom.minidom import parse, parseString
-from xml.etree.ElementTree import tostring
 from bs4 import BeautifulSoup
 
 if ((sys.platform) == "win32"):
---
+++ coretest.py
@@ -0,0 +1,18 @@
+#!/web/tdymkru/python/bin/python2.6
+# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
+import sys,os
+import cgi
+import cgitb; cgitb.enable()
+from bs4 import BeautifulSoup
+
+if ((sys.platform) == "win32"):
+    print ("")
+    sys.stdout = open('temp.html', 'w')
+else:
+    print ("Content-Type: text/html; charset=utf-8")
+    print ("")
+    
+print("<!DOCTYPE html>")
+
+print("test")
