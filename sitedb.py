8258d14e0978bd3b9cd866ee3be0b19b0d36d5eb
vgulaev <vgulaev@yandex.ru>
2012-12-26 11:45:33 +0600

    First db file

--- sitedb.py
+++ sitedb.py
@@ -1,21 +1,35 @@
 #!/web/tdymkru/python/bin/python2.6
 # -*- coding: utf-8 -*-
 # This Python file uses the following encoding: utf-8
-import sqlite3
+import sys, os
+import cgitb; cgitb.enable()
+sys.path.insert(0, os.path.expanduser('~/site/python'))
+import sqlalchemy
+from sqlalchemy import create_engine
 
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
+print ("Content-Type: text/html; charset=utf-8")
+print ("")
+    
+print("<!DOCTYPE html>")
 
-# Create table
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
+print(sqlalchemy.__version__)
 
-# Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+engine = create_engine('mysql://tdymkru:8awzVTe1@localhost:3306/tdymkru')
 
-# Save (commit) the changes
-conn.commit()
 
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
-conn.close()
+# conn = sqlite3.connect('example.db')
+# c = conn.cursor()
+
+# # Create table
+# c.execute('''CREATE TABLE stocks
+             # (date text, trans text, symbol text, qty real, price real)''')
+
+# # Insert a row of data
+# c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# # Save (commit) the changes
+# conn.commit()
+
+# # We can also close the connection if we are done with it.
+# # Just be sure any changes have been committed or they will be lost.
+# conn.close()
