78f496be64aa4994dff3a3327091067421b301ec
vgulaev <vgulaev@yandex.ru>
2012-12-31 19:04:40 +0600

    Commit for commit

--- onlineservices/itemtable.py
+++ onlineservices/itemtable.py
@@ -1,4 +1,6 @@
 #!/web/tdymkru/python/bin/python2.6
 # -*- coding: utf-8 -*-
 # This Python file uses the following encoding: utf-8
+print ("Content-Type: text/html; charset=utf-8")
+print ("")
 print "Hello!!!"
