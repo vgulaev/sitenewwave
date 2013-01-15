e3edf6deeaf22b6e8a8204b7b3ffccb78a599323
alexander fedorov <elf607@ya.ru>
2013-01-15 14:32:01 +0600

    script for path changing

---
+++ htmlstaticcontent/changePathToimg.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import sys,os,re
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+
+filename = "028_products_krovl_vodostochnye_sistemy_/index.html"
+
+html_file_name = os.path.join(_PATH, filename)
+html_file = open(html_file_name,'r')
+html_file_string = html_file.read()
+html_file.close()
+
+p6 = re.compile("/upload[\w\-\s/]+\.jpg|src=\"[\w\-\s/]+\.png|src=\"[\w\-\s/]+\.gif|src=\"[\w\-\s/]+\.ico")
+
+imgpaths = p6.findall(html_file_string)
+
+for path in imgpaths:
+    p4 = re.compile("[\w\-\s]+\.jpg|[\w\-\s]+\.png|[\w\-\s]+\.gif|[\w\-\s]+\.ico")
+    imgname = p4.findall(path)
+
+    for img in imgname:
+
+        html_file_string = html_file_string.replace(path, "/htmlstaticcontent/unknown_images/"+img)
+
+        print path, " --> ", img
+
+
+html_file_name = os.path.join(_PATH, filename)
+html_file = open(html_file_name,'w')
+
+html_file.write(html_file_string)
+html_file.close()
