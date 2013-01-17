f713fd69c3aebb823140098406e1f77d295c4ff5
alexander fedorov <elf607@ya.ru>
2013-01-17 11:48:19 +0600

    fixing pda sending

--- 1cengine/site/pda/mail.php
+++ 1cengine/site/pda/mail.php
@@ -1,16 +1,15 @@
 <?php 
-                require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+
+$fp = fopen("../../../locate/ru/templates/mainpage_template.html","r");
+$template_string = fread($fp, filesize("../../../locate/ru/templates/mainpage_template.html"));
+$qq = '<link rel="stylesheet" type="text/css" href="/mainpage_template.css" media="all" />';
+$titleTamplate = '<title> Главная страница </title>';
+$title = '<title> Тримет мобильный заказ </title>';
+$template_string = str_replace($titleTamplate, $title, $template_string); 
+echo $template_string;
+fclose($fp);
+
 ?>
-
-<?php
-$APPLICATION->IncludeFile(
-                SITE_TEMPLATE_PATH . '/include_areas/page_standart_header.php',
-                array(),
-                array('MODE' => 'php')
-);
-
-?>
-
 <?php
     $filename = $_POST['file_format']; //Имя файла для прикрепления
     $to = $_POST['mail_to']; //Кому
@@ -56,107 +55,8 @@ $APPLICATION->IncludeFile(
 
 <?php 
 
-$APPLICATION->IncludeFile(
-                SITE_TEMPLATE_PATH . '/include_areas/page_standart_pre_footer.php',
-                array('css_class' => $cssClass),
-                array('MODE' => 'php')
-); ?>
-
-                <div id="footer">
-                                <a href="#top" id="bottom" tabindex="3" title="наверх страницы"></a>
-                                <div id="post-cont" class="clear">&nbsp;</div>
-
-<?php
-$APPLICATION->IncludeFile(
-                '/copy.php',
-                array('SET_TITLE' => 'N',),
-                array('MODE' => 'php')
-); ?>
-
-
-
-                                <div class="fright">
-                                                <form id="search2" method="get" action="/search/">
-                                                                <fieldset class="input-field">
-                                                                                <input id="q" name="q" value="Поиск" type="text" onfocus="if(this.value=='Поиск') this.value=''" onblur="if(this.value=='') this.value='Поиск'" />
-                                                                </fieldset>
-                                                </form>
-
-                                                <div id="obj-counter">
-                                                                <!-- LiveInternet counter -->
-                                                                <script type="text/javascript"><!--//--><![CDATA[//><!--
-                                                                document.write('<a href="http://www.liveinternet.ru/click" target="_blank">' +
-                                                                '<img src="http://counter.yadro.ru/hit?t14.1;r' + 
-                                                                escape(document.referrer) + 
-                                                                ( (typeof(screen)=='undefined') ? '' : ';s' + screen.width + '*' + screen.height + '*' + (screen.colorDepth ? screen.colorDepth : screen.pixelDepth) ) +
-                                                                ';u' + escape(document.URL) + 
-                                                                ';h' + escape(document.title.substring(0,80)) + 
-                                                                ';' + Math.random() +
-                                                                '" alt="" title="LiveInternet: показано количество просмотров и посетителей" ' +
-                                                                'border="0" width="88" height="31" /><\/a>');
-                                                                //--><!]]></script>
-                                                                <!--/LiveInternet-->
-                                                                <!-- GoogleAnalysts counter-->
-                                                                <script type="text/javascript">
-                                                                var _gaq = _gaq || [];
-                                                                _gaq.push(['_setAccount', 'UA-33206471-1']);
-                                                                _gaq.push(['_trackPageview']);
-                                                                (function() {
-                                                                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                                                                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                                                                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-                                                                })();
-                                                                </script> 
-                                                                <!-- /GoogleAnalysts -->
-                                                                <!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-(function (d, w, c) {
-                (w[c] = w[c] || []).push(function() {
-                                try {
-                                                w.yaCounter15882208 = new Ya.Metrika({id:15882208,
-                                                                                clickmap:true,
-                                                                                trackLinks:true, webvisor:true});
-                                } catch(e) {}
-                });
-         
-                var n = d.getElementsByTagName("script")[0],
-                                s = d.createElement("script"),
-                                f = function () { n.parentNode.insertBefore(s, n); };
-                s.type = "text/javascript";
-                s.async = true;
-                s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-                if (w.opera == "[object Opera]") {
-                                d.addEventListener("DOMContentLoaded", f);
-                } else { f(); }
-})(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="//mc.yandex.ru/watch/15882208" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter --> 
-                                                </div>
-
-                                                <!-- <div id="autor">
-                                                                <p>Создание сайта</p>
-                                                                <a href="http://russianmultimedia.ru/contacts.php#feedback" title="Создание сложных мультимедийных продуктов: сайтов публичных компаний, 3d-видео и стерео роликов, мультимедийных инсталляций" target="_blank">Russian multimedia company</a>
-                                                < -->/div>
-                                </div>
-
-<?php $APPLICATION->IncludeComponent(
-                'bitrix:menu',
-                'bottom',
-                array(
-                                "ROOT_MENU_TYPE"        => "top",
-                                "MAX_LEVEL"             => "1",
-                                "CHILD_MENU_TYPE"       => "left",
-                                "USE_EXT"               => "N",
-                                "ALLOW_MULTI_SELECT"    => "N",
-                                "MENU_CACHE_TYPE"       => "N",
-                                "MENU_CACHE_TIME"       => "3600",
-                                "MENU_CACHE_USE_GROUPS" => "Y",
-                                "MENU_CACHE_GET_VARS"   => Array()
-                ),
-                false
-); ?>
-
-                </div> <!-- /footer -->
+// $fp = fopen("../../mainfooter_template.html","r");
+echo file_get_contents("../../../locate/ru/templates/mainfooter_template.html");
+// fclose($fp);
+?>
 </body>
