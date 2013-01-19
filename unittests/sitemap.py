ebfbe1c1048dffe2708331b2ba087118819b1780
Valentin <vgulaev@yandex.ru>
2013-01-19 22:32:51 +0600

    First test for trimet.ru

---
+++ unittests/sitemap.py
@@ -0,0 +1,21 @@
+#!/web/trimetru/python/bin/python2.6
+# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
+import sys, os
+import cgi
+import cgitb; cgitb.enable()
+sys.path.insert(0, os.path.expanduser('~/site/python'))
+from bs4 import BeautifulSoup
+from urllist import trimeturls
+import Cookie
+
+if ((sys.platform) == "win32"):
+    print ("")
+    sys.stdout = open('temp.html', 'w')
+else:
+    print ("Content-Type: text/html; charset=utf-8")
+    print ("")
+    
+print("<!DOCTYPE html>")
+
+print("this py test sitemap");
