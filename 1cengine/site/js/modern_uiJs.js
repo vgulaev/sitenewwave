dc58b5942791fcd993d964839740fae1892bb547
alexander fedorov <elf607@ya.ru>
2013-01-24 13:16:28 +0600

    fixing delivery fixes #23

--- 1cengine/site/js/modern_uiJs.js
+++ 1cengine/site/js/modern_uiJs.js
@@ -1885,107 +1885,115 @@ $(document).ready( function(){
         $("#destination").autocomplete( "option", "source", "getStreet.php?town="+townS )
     })
 
-    $("#townSelect").change( function(){
-        if($("#townSelect :selected").val()!="72000001"){
+    $("#townSelect").change(function() {
+        if($("#townSelect :selected").val() != "72000001") {
             $("#carry [value='Длинномер']").attr("selected", "selected")
             $("#carry").attr("disabled", "disabled")
 
             $("#delivery_cost").empty()
             var d_price = $("#townSelect :selected").attr("price")
-            if($("#selfCarry").is(":checked")==false){
-                var totalCost = $("#SumAll").attr("name")
-                totalCost = ((totalCost-0)+(d_price-0)).toFixed(2)
-                totalCost = totalCost.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+totalCost.split('.')[1]
+            if($("#selfCarry").is(":checked") == false) {
+
+                var totalCost = getTotalCost(1)
+
                 $("#SumAll").empty()
                 $("#SumAll").append(totalCost)
 
             }
-            d_price = (d_price-0).toFixed(2)
-            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+d_price.split('.')[1]
+            d_price = (d_price - 0).toFixed(2)
+            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ') + '.' + d_price.split('.')[1]
             $("#delivery_cost").append(d_price)
+            $("#delivery_cost").attr("name", d_price)
             $("#SumDelivery").html(d_price)
 
-        } else if ($("#townSelect :selected").val()=="72000001") {
+        } else if($("#townSelect :selected").val() == "72000001") {
             $("#carry").removeAttr("disabled")
 
-            if($("#carry :selected").val()=="--"){
+            if($("#carry :selected").val() == "--") {
                 $("#carry [value='Газель']").attr("selected", "selected")
             }
 
             $("#delivery_cost").empty()
             var d_price = $("#carry :selected").attr("price")
-            if($("#selfCarry").is(":checked")==false){
-                var totalCost = $("#SumAll").attr("name")
-                totalCost = ((totalCost-0)+(d_price-0)).toFixed(2)
-                totalCost = totalCost.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+totalCost.split('.')[1]
+            if($("#selfCarry").is(":checked") == false) {
+
+                var totalCost = getTotalCost(1)
+
                 $("#SumAll").empty()
                 $("#SumAll").append(totalCost)
             }
-            d_price = (d_price-0).toFixed(2)
-            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+d_price.split('.')[1]
+            d_price = (d_price - 0).toFixed(2)
+            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ') + '.' + d_price.split('.')[1]
             $("#delivery_cost").append(d_price)
+            $("#delivery_cost").attr("name", d_price)
             $("#SumDelivery").html(d_price)
 
-             
+
         }
         $("#destination").attr('value', '');
     })
-    $("#carry").change( function(){
-        if($("#townSelect :selected").val()=="72000001"){
+    $("#carry").change(function() {
+        if($("#townSelect :selected").val() == "72000001") {
             $("#delivery_cost").empty()
             var d_price = $("#carry :selected").attr("price")
-            if($("#selfCarry").is(":checked")==false){
-                var totalCost = $("#SumAll").attr("name")
-                totalCost = ((totalCost-0)+(d_price-0)).toFixed(2)
-                nAll = ((totalCost/118)*18).toFixed(2)
-                nAll = nAll.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+nAll.split('.')[1]
-                totalCost = totalCost.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+totalCost.split('.')[1]
+            if($("#selfCarry").is(":checked") == false) {
+
+                var totalCost = getTotalCost(1)
+
                 $("#SumAll").empty()
                 $("#SumAll").append(totalCost)
             }
-            d_price = (d_price-0).toFixed(2)
-            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+d_price.split('.')[1]
+            d_price = (d_price - 0).toFixed(2)
+            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ') + '.' + d_price.split('.')[1]
             $("#delivery_cost").append(d_price)
+            $("#delivery_cost").attr("name", d_price)
             $("#SumDelivery").html(d_price)
         }
     })
-    $("#selfCarry").change( function(){
+    $("#selfCarry").change(function() {
         // alert(1)
         var d_price = $("#delivery_cost").html().replace(/\s/g, "")
-        if($("#selfCarry").is(":checked")){
-            $(".withDelivery").hide()
-            $(".withoutDelivery").show()
+        if($("#selfCarry").is(":checked")) {
             // alert(1)
-            var totalCost = $("#SumAll").attr("name")
-            totalCost = (totalCost-0).toFixed(2)
-            nAll = ((totalCost/118)*18).toFixed(2)
-            nAll = nAll.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+nAll.split('.')[1]
-            totalCost = totalCost.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+totalCost.split('.')[1]
-            $("#SumAll").empty()
-            $("#SumAll").append(totalCost)
-            $("#SumDelivery").empty()
-        } 
-    })
-    $("#toDeliver").change( function(){
-        var d_price = $("#delivery_cost").html().replace(/\s/g, "")
-        if($("#toDeliver").is(":checked")){
-            $(".withDelivery").show()
-            $(".withoutDelivery").hide()
-            // alert(2)
-            var totalCost = $("#SumAll").attr("name")
-            totalCost = ((totalCost-0)+(d_price-0)).toFixed(2)
-            nAll = ((totalCost/118)*18).toFixed(2)
-            nAll = nAll.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+nAll.split('.')[1]
-            totalCost = totalCost.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+totalCost.split('.')[1]
+
+            var totalCost = getTotalCost(0)
             
             $("#SumAll").empty()
             $("#SumAll").append(totalCost)
-            d_price = (d_price-0).toFixed(2)
-            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ')+'.'+d_price.split('.')[1]
+            $("#SumDelivery").empty()
+            $("#delivery_cost").empty()
+        }
+    })
+    $("#toDeliver").change(function() {
+        var d_price = $("#delivery_cost").attr("name").replace(/\s/g, "")
+        if($("#toDeliver").is(":checked")) {
+            // alert(2)
+            
+            var totalCost = getTotalCost(1)
+
+            $("#SumAll").empty()
+            $("#SumAll").append(totalCost)
+            d_price = (d_price - 0).toFixed(2)
+            d_price = d_price.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ') + '.' + d_price.split('.')[1]
             // $("#delivery_cost").append(d_price)
             $("#SumDelivery").html(d_price)
         }
     })
+
+    function getTotalCost(d){
+        var totalCost = $("#SumAll").attr("name")
+        if(d==1){
+            totalCost = ((totalCost - 0) + (d_price - 0)).toFixed(2)
+        } else {
+            totalCost = (totalCost - 0).toFixed(2)
+        }
+        
+        // nAll = ((totalCost / 118) * 18).toFixed(2)
+        // nAll = nAll.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ') + '.' + nAll.split('.')[1]
+        
+        totalCost = totalCost.split('.')[0].replace(/(\d{1,3})(?=(?:\d{3})+$)/g, '$1 ') + '.' + totalCost.split('.')[1]
+        return totalCost
+    }
 
 })
 
