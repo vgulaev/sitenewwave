ec8c1c39b9a6f82255b689205ea925e8b3d38d78
vgulaev <vgulaev@yandex.ru>
2013-01-28 13:46:55 +0600

    Don't forget correct header

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 #!c:/Python27/python.exe
 # -*- coding: utf-8 -*-
 # This Python file uses the following encoding: utf-8
+#-- #!/web/trimetru/python/bin/python2.6
 import sys, os
 import cgi
 import cgitb; cgitb.enable()
