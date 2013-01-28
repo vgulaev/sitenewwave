63b7d9bd90bdf449487dde2e7378d91368053732
vgulaev <vgulaev@yandex.ru>
2013-01-28 12:50:44 +0600

    Test that Python working correctly

---
+++ unittests/testpythonwork.py
@@ -0,0 +1,8 @@
+#!c:/Python27/python.exe
+# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
+
+print ("Content-Type: text/html; charset=utf-8")
+print ("")
+    
+print("hello!!!")
