6f5b09a2ee2bf5f3c516755c009c4c6c7ea8cedf
vgulaev <vgulaev@yandex.ru>
2013-01-28 14:53:33 +0600

    Commit for commit

---
+++ sitemap.py
@@ -0,0 +1,63 @@
+#!c:/Python27/python.exe
+# -*- coding: utf-8 -*-
+# This Python file uses the following encoding: utf-8
+# -- #!/web/trimetru/python/bin/python2.6
+import sys, os
+import cgi
+import cgitb; cgitb.enable()
+sys.path.insert(0, os.path.expanduser('~/site/python'))
+from bs4 import BeautifulSoup
+
+debugmode = True
+if ((sys.platform) == "win32"):
+    # print ("")
+    # sys.stdout = open('temp.html', 'w')
+    print ("Content-Type: text/html; charset=utf-8")
+    print ("")
+else:
+    print ("Content-Type: text/html; charset=utf-8")
+    print ("")
+    
+print("<!DOCTYPE html>")
+
+def makecontent(path=None):
+    # print path
+    soup = BeautifulSoup(open("locate/ru/templates/mainpage_template.html"))
+    if (debugmode == True):
+        soup.html.noscript.extract()
+        nodes = soup.html.body("script")
+        for currentelement in nodes:
+            currentelement.extract()
+    soupForImport = BeautifulSoup("""<html><table><thead>
+<th>№</th>
+<th>Link</th>
+<th>Changefreq</th>
+</thead>
+<tbody>
+</tbody>
+</table></html>""")
+    soupSiteMap = BeautifulSoup(open("sitemap.xml"), "xml")
+    nodes = soupSiteMap.find_all("loc")
+    counter = 0;
+    tbody = soupForImport.html.table.tbody
+    for el in nodes:
+        counter = counter + 1
+        tag_tr = soupForImport.new_tag("tr")
+        tag_td = soupForImport.new_tag("td")
+        tag_td.append(str(counter))
+        tag_tr.append(tag_td)
+        tag_td = soupForImport.new_tag("td")
+        tbody.append(tag_tr)       
+    
+    # table = soupForImport.new_tag("table")
+    
+    # soupForImport.html.append(table)
+    # soupForImport.append()
+    soupFooter = BeautifulSoup(open("locate/ru/templates/mainfooter_template.html"))
+    node = soupFooter.find("footer", {"id": "footer"})
+    soup.html.body.append(node)
+    print(soupForImport.prettify("utf-8"))
+    # print(soup.prettify("utf-8"))
+
+makecontent()
+# print("Hello!!!");
