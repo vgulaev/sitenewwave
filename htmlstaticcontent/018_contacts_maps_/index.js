ff1d3cd1b0e55df03a2d4e836ee495f147021625
alexander fedorov <elf607@ya.ru>
2013-01-30 12:57:05 +0600

    fixing styles... for issue #27

--- htmlstaticcontent/018_contacts_maps_/index.js
+++ htmlstaticcontent/018_contacts_maps_/index.js
@@ -1,13 +1,15 @@
-var lbOpt = {
-	overlayOpacity: '0.3',
-	imageLoading: '/img/ajax-loader-t.gif',
-	imageBtnClose: '/img/ajax-close-2.gif'
-};
-$('.map-1').lightBox(lbOpt);
-$('.map-2').lightBox(lbOpt);
-$('.map-3').lightBox(lbOpt);
-$('.map-4').lightBox(lbOpt);
-$('.map-1-1').lightBox(lbOpt);
-$('.map-2-1').lightBox(lbOpt);
-$('.map-3-1').lightBox(lbOpt);
-$('.map-4-1').lightBox(lbOpt);
+$(document).ready( function(){
+	var lbOpt = {
+		overlayOpacity: '0.3',
+		imageLoading: '/img/ajax-loader-t.gif',
+		imageBtnClose: '/img/ajax-close-2.gif'
+	};
+	$('.map-1').lightBox(lbOpt);
+	$('.map-2').lightBox(lbOpt);
+	$('.map-3').lightBox(lbOpt);
+	$('.map-4').lightBox(lbOpt);
+	$('.map-1-1').lightBox(lbOpt);
+	$('.map-2-1').lightBox(lbOpt);
+	$('.map-3-1').lightBox(lbOpt);
+	$('.map-4-1').lightBox(lbOpt);
+})
