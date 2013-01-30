184430f26a4cd7da4a8fe920695f96cd866362d1
alexander fedorov <elf607@ya.ru>
2013-01-30 12:39:53 +0600

    fancy map view with lightbox on issue #27

---
+++ htmlstaticcontent/018_contacts_maps_/index.js
@@ -0,0 +1,13 @@
+var lbOpt = {
+	overlayOpacity: '0.3',
+	imageLoading: '/img/ajax-loader-t.gif',
+	imageBtnClose: '/img/ajax-close-2.gif'
+};
+$('.map-1').lightBox(lbOpt);
+$('.map-2').lightBox(lbOpt);
+$('.map-3').lightBox(lbOpt);
+$('.map-4').lightBox(lbOpt);
+$('.map-1-1').lightBox(lbOpt);
+$('.map-2-1').lightBox(lbOpt);
+$('.map-3-1').lightBox(lbOpt);
+$('.map-4-1').lightBox(lbOpt);
