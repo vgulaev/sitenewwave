f36c1cd587da6a7e164db15e25c647edf4bbcbcc
alexander fedorov <elf607@ya.ru>
2013-02-01 14:08:41 +0600

    urllist updated

--- urllist.py
+++ urllist.py
@@ -58,3 +58,4 @@ trimeturls.append(urltype("products/request", "htmlstaticcontent/037_products_re
 trimeturls.append(urltype("products/krovl/dobornye-jelementy", "htmlstaticcontent/046_products_krovl_dobornye-jelementy_/"))
 
 trimeturls.append(urltype("kabinet", "1cengine/kabinet/"))
+trimeturls.append(urltype("kabinet/orders", "1cengine/kabinet_orders/"))
