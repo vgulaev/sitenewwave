5e3671d05206543e3244494741a5a141220cce27
alexander fedorov <elf607@ya.ru>
2013-02-07 13:18:25 +0600

    working on #42

--- 1cengine/kabinet_authorization/index.js
+++ 1cengine/kabinet_authorization/index.js
@@ -1,21 +1,42 @@
 $(document).ready( function (){
     $(".enterButton").click( function(){
+        if($(".newUser").attr("checked") == "checked"){
+            newUser()
+        } else {
+            loginUser()
+        }
+    })
+
+    $(".newUser").change( function(){
+        if($(".newUser").attr("checked") == "checked"){
+            $(".passwdRepeatTr").slideDown("400")
+        } else {
+            $(".passwdRepeatTr").slideUp("400")
+        }
+    })
+
+    function loginUser(){
         passwd = $(".passwdInput").attr("value")
         email = $(".emailInput").attr("value")
         if(passwd!="" && email!=""){
-            $.ajax({
-                type: "POST",
-                url: "/1cengine/kabinet_authorization/user.py",
-                async: false,
-                data: "funkt=checkUser&email=" + email + "&passwd=" + hex_sha256(passwd),
-                success: function(html) {
-                    ret = html
-                    alert(ret)
-                }
-            });
-            // alert(hex_sha256(passwd))    
+            passwd = hex_sha256(passwd)
+            $(".passwdInput").attr("value",passwd) 
+            $("#regForm").submit()  
         }
-        
-    })
+    }
+
+    function newUser(){
+        passwd = $(".passwdInput").attr("value")
+        passwdRepeat = $(".passwdInputRepeat").attr("value")
+        email = $(".emailInput").attr("value")
+        if(passwd!="" && email!=""){
+            if(passwd==passwdRepeat){
+                passwd = hex_sha256(passwd)
+                $(".passwdInput").attr("value",passwd) 
+                $("#regForm").submit()
+            }
+        }
+    }
+
 })
 
