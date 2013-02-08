b8d87a0132acc185338c428fd01580e61c5536a7
alexander fedorov <elf607@ya.ru>
2013-02-08 13:06:29 +0600

    making 1c integration for #42

---
+++ 1cengine/kabinet_authorization/1c_user_interaction.py
@@ -0,0 +1,52 @@
+#!/web/trimetru/python/bin/python2.6
+# -*- coding:utf-8 -*-
+
+import sys, os
+import cgi
+import cgitb; cgitb.enable()
+
+sys.path.insert(0, os.path.expanduser('~/site/python'))
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# print ("Content-Type: text/html; charset=utf-8\n")
+
+import json
+from suds.client import Client
+from suds.cache import DocumentCache
+
+import logging
+logging.basicConfig(level=logging.INFO)
+if __debug__:
+    logging.getLogger('suds.client').setLevel(logging.DEBUG)
+else:
+    logging.getLogger('suds.client').setLevel(logging.CRITICAL)
+
+_DEVELOPING_ADDRESS_ = "http://192.168.194.14/DemoTrimet/ws/"
+_PRODUCTION_ADDRESS_ = "http://195.239.221.58:30080/DemoTrimet/ws/"
+
+if "dev" in os.environ["SERVER_NAME"]:
+	_CURRENT_ADDRESS_ = _DEVELOPING_ADDRESS_
+else:
+	_CURRENT_ADDRESS_ = _PRODUCTION_ADDRESS_
+
+class User1C():
+	def __init__(self):
+		pass
+
+	def register_user_1c(self, email, passwd):
+	    client = Client(_CURRENT_ADDRESS_+"Register.1cws?wsdl", location = _CURRENT_ADDRESS_+"Register.1cws")
+	    client.set_options(cache=DocumentCache())
+
+	    username = ""
+	    fullname = ""
+
+	    result = client.service.AddUser(email,passwd,email,fullname)
+
+	    return result
+
+	def 
+
+
+def __main__(funkt=False):
+	return eval(funkt)
