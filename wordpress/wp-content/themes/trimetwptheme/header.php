62fa23299cda54d2b6cb52ae597ac970058e05e2
vgulaev <vgulaev@yandex.ru>
2013-02-13 18:26:16 +0600

    Beautiful header

--- wordpress/wp-content/themes/trimetwptheme/header.php
+++ wordpress/wp-content/themes/trimetwptheme/header.php
@@ -23,6 +23,14 @@
     $template_string = str_replace("</body>", "", $template_string);
     $template_string = str_replace("</html>", "", $template_string);
     
+    /*$border_head = strpos($template_string, "</head>");
+    $template_string_befor_close_head = substr($template_string, 0, $border_head);
+    $template_string_after_close_head = substr($template_string, $border_head);
+    //separated for call 
+    //    
+    echo $template_string_befor_close_head;
+    wp_head();
+    echo $template_string_after_close_head;*/
     echo $template_string;
 ?>
 	<div id="main" class="wrapper">
