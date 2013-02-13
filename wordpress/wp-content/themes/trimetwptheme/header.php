36d69c6d84e87bc5f49ed4141c0356263f561a1f
vgulaev <vgulaev@yandex.ru>
2013-02-13 15:56:04 +0600

    Commit for commit

--- wordpress/wp-content/themes/trimetwptheme/header.php
+++ wordpress/wp-content/themes/trimetwptheme/header.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 /**
  * The Header for our theme.
  *
@@ -9,46 +9,56 @@
  * @since Twenty Twelve 1.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="ie ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8)  ]><!-->
-<html <?php language_attributes(); ?>>
-<!--<![endif]-->
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php wp_head(); ?>
-</head>
+<?php
 
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
+$wwwdir = "C:/Users/Administrator/workspace/sitenewwave/";
+$fp = fopen($wwwdir."/locate/ru/templates/mainpage_template.html","r");
+$template_string = fread($fp, filesize($wwwdir."/locate/ru/templates/mainpage_template.html"));
+fclose($fp);
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
+$titleTemplate = '<title> Тримет </title>';
+$keywordsTemplate = '"металлопрокат, профнастил, металлосайдинг, купить, онлайн, тюмень, арматура, балка, швеллер, трубы, угол, штрипс, квадрат, круг, лист, проволока" />';
 
-		<?php $header_image = get_header_image();
-		if ( ! empty( $header_image ) ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" class="header-image" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="" /></a>
-		<?php endif; ?>
-	</header><!-- #masthead -->
+if(isset($_GET["ref"])){
+    $ntitle = str_replace("\\\"", "\"",$_GET["ref"]);
+    $ntitle = str_replace("(", "", $ntitle);
+    $ntitle = str_replace(")", "", $ntitle);
+    $ntitle = str_replace("\"", "", $ntitle);
 
+    $title = '<title> '.$ntitle.' купить онлайн | Тримет ООО </title>';
+
+    $keywordsArray = explode(" ", $ntitle);
+    $keywordsString = "";
+    foreach($keywordsArray as $keyword){
+        $keywordsString .= $keyword.", ";
+    }
+    $keywordsString .= "купить, онлайн, тюмень";
+
+    $keywords = '"'.$keywordsString.'" />';
+    
+    // $APPLICATION->SetPageProperty("keywords", "".$_GET["ref"].", купить, тримет, тюмень");
+    // $APPLICATION->SetPageProperty("description", "Купить ".$_GET["ref"]." в компании Тримет");
+} else {
+    $title = '<title> Купить Online </title>';
+    $keywords = $keywordsTemplate;
+    // $APPLICATION->SetPageProperty("keywords", "металлопрокат, профнастил, металлосайдинг, купить, онлайн, тюмень, арматура, балка, швеллер, трубы, угол, штрипс, квадрат, круг, лист, проволока");
+    // $APPLICATION->SetPageProperty("description", "Покупка металлосайдинга, профнастила, металлопроката в Тюмени онлайн");
+}
+
+$template_string = str_replace($titleTemplate, $title, $template_string); 
+$template_string = str_replace($keywordsTemplate, $keywords, $template_string); 
+$template_string = str_replace("</body>", "", $template_string); 
+$template_string = str_replace("</html>", "", $template_string); 
+
+
+echo $template_string;
+
+// Более не нужный кусок кода
+// if(isset($_GET["ref"])){
+//     if(strstr($_GET["ref"], "кастом")!=false){
+
+//         header( 'Refresh: 0; url=http://trimet.ru/404.html?ref='.urlencode($_GET["ref"]).'' );
+//     }
+// }
+?>
 	<div id="main" class="wrapper">
