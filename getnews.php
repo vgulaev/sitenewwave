47000b01932e82314ce0ea5eefd3990851914023
alexander fedorov <elf607@ya.ru>
2013-02-20 15:42:59 +0600

    other update for newslist, using wp api

--- getnews.php
+++ getnews.php
@@ -1,7 +1,8 @@
 <?php
     require_once("wordpress/wp-load.php");
     //echo "Hello";
-	$recent_posts = wp_get_recent_posts();
+    $args = array( 'numberposts' => '5' );
+	$recent_posts = wp_get_recent_posts( $args );
 	$k = 0;
     foreach( $recent_posts as $recent ){
         if ($k == 0) {
@@ -11,8 +12,5 @@
         echo '<li><span>'.$recent["post_date"].'</span><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
         }
         $k = $k + 1;
-        if( $k >= 5 ){
-            break; // Больше пяти новостей смотрится уже плохо и не красиво
-        }
 	}
 ?>
