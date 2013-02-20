5728787a83b53eba04920c430456b4764d00166e
alexander fedorov <elf607@ya.ru>
2013-02-20 11:09:43 +0600

    close #56

--- htmlstaticcontent/linkCheck.py
+++ htmlstaticcontent/linkCheck.py
@@ -57,7 +57,7 @@ for dirname in dirList:
             if link_array[link]>0:
                 print link, " : ", link_array[link]
 
-                html_file_string.replace(link, link+' rel="nofollow"', 1)
+                html_file_string = html_file_string.replace(link, link+' rel="nofollow"', 1)
 
         
         # p6 = re.compile("/upload[\w\-\s_/\.]+jpg|/upload[\w\-\s_/\.]+png|/upload[\w\-\s_/\.]+gif|/upload[\w\-\s_/\.]+ico")
