bddac89ceaabe565490323a1e2cafc0d8eb920ad
vgulaev <vgulaev@yandex.ru>
2013-03-09 19:35:20 +0600

    Commit for commit

--- interactivesearch/is.js
+++ interactivesearch/is.js
@@ -1,7 +1,18 @@
+waitanswer = false;
+
 function say_hello() {
     alert("Hello");
 }
 
+function giveoptions() {
+    
+};
+
 $(document).ready(function() {
-    alert("Hello"); 
+    //alert("Hello");
+    $("#a11").keyup(function() {
+        //alert('Handler for .change() called.');
+        $("#output").html($("#a11").val());
+        giveoptions();
+        });
 });
