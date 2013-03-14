9f59d680493bb5e8356760b4063f4257f8a3a2ef
alexander fedorov <elf607@ya.ru>
2013-03-14 13:56:38 +0600

    making christmass, making christmass, it's so fun... working under #90 making seotags

---
+++ 1cengine/site/test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<?php 
+    $fp = fopen("../../locate/ru/templates/mainpage_template.html","r");
+    $template_string = fread($fp, filesize("../../locate/ru/templates/mainpage_template.html"));
+    fclose($fp);
+
+    $titleTemplate = '<title> Тримет </title>';
+    $keywordsTemplate = '"металлопрокат, профнастил, металлосайдинг, купить, онлайн, тюмень, арматура, балка, швеллер, трубы, угол, штрипс, квадрат, круг, лист, проволока" />';
+
+
+    $template_string = str_replace($titleTemplate, $title, $template_string); 
+    $template_string = str_replace($keywordsTemplate, $keywords, $template_string); 
+    $template_string = str_replace("</body>", "", $template_string); 
+    $template_string = str_replace("</html>", "", $template_string); 
+
+
+    echo $template_string;
+
+    $fp = fopen("seotags.json","r");
+    $json_string = fread($fp, filesize("seotags.json"));
+    fclose($fp);
+
+    $tags_obj=json_decode($json_string);
+    // echo $json_string;
+    // print_r($tags_obj);
+    echo "<ul>Арматура";
+    echo "<li>" . $tags_obj->Арматура->url . "</li>";
+    echo "<li>" . $tags_obj->Арматура->title . "</li>";
+    echo "<li>" . $tags_obj->Арматура->description . "</li>";
+    echo "<li>" . $tags_obj->Арматура->text . "</li>";
+    echo "</ul>";
+
+?>
