9f7d3ee1f7faf11231b099a8b081b54d7951f07f
alexander fedorov <elf607@ya.ru>
2013-04-01 09:57:31 +0600

    some cleaning

--- wordpress/wp-content/themes/mobile_pack_base/index.php
+++ wordpress/wp-content/themes/mobile_pack_base/index.php
@@ -13,7 +13,6 @@
         <!--script src="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script-->
         <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
         <!-- // <script src="lib/frameworks/jquerymobile/1.3.0/jquery.mobile-1.3.0.min.js"></script> -->
-        <script src="m/my.js"></script>
         <!-- User-generated css -->
         <style>
         </style>
@@ -52,22 +51,15 @@
             <?php 
             while (have_posts()) {
                 the_post();
-                // print '<div class="post" id="post-' . get_the_ID() . '">';
-                if(is_single() || is_page()) {
-                    print '<h1>' . get_the_title() . '</h1>';
-                    wpmp_theme_post_single();
-                } else {
-                    print '<li data-theme="c">';
-                    print '<a href="#article'.get_the_ID().'" data-transition="slide">' . get_the_title() . '</a>';
-                    // wpmp_theme_post_summary();
-                    print '</li>';
-              }
+
+                print '<li data-theme="c">';
+                print '<a href="#article'.get_the_ID().'" data-transition="slide">' . get_the_title() . '</a>';
+                print '</li>';
+
             } 
             ?>
         </ul>
-        <!--select id="sdsfsd" name="select-choice-1">
-            <option value="null">Выберите группу товаров</option>
-        </select-->
+
         
     </div>
 </div>
@@ -75,13 +67,13 @@
 
 
 <?php 
-    while (have_posts()) {
-        the_post();
-        // print '<div class="post" id="post-' . get_the_ID() . '">';
-        print '<div data-role="page" id="article'.get_the_ID().'">';
-        ?>
+while (have_posts()) {
+    the_post();
+    // print '<div class="post" id="post-' . get_the_ID() . '">';
+    print '<div data-role="page" id="article'.get_the_ID().'">';
+    ?>
 
-        <div data-role="content">
+    <div data-role="content">
         <?php 
             print '<h1>' . get_the_title() . '</h1>';
             print get_the_content(); 
@@ -92,69 +84,16 @@
 
             </li>
         </ul>
-        <!--a id=" ButtonEx" data-role="button" href="#page1" onclick="doSomething(); return false" rel="external"> Button </a-->
         
 
     </div>
 </div>
     <?php
-        // print '<li data-theme="c">';
-        // print '<a href="#article'.get_the_ID().'" data-transition="slide">' . get_the_title() . '</a>';
-        // // wpmp_theme_post_summary();
-        // print '</li>';
-  
 } 
 
-
-
-function wpmp_theme_post_single() {
-  wpmp_theme_post(true);
-  print '<p class="metadata">'; previous_post_link(__('Previous post:', 'wpmp') . ' %link'); print '<br />'; next_post_link(__('Next post:', 'wpmp') . ' %link'); print '</p>';
-  if(!function_exists('wpmp_transcoder_is_last_page') || wpmp_transcoder_is_last_page()) {
-    global $post;
-    if (!$post->comment_status=='open') {
-      print '<p class="metadata">' . __('Comments are closed for this post.', 'wpmp') . '</p>';
-      print '</div>';
-    } else {
-      print '</div>';
-      comments_template();
-    }
-  }
-}
-
-function wpmp_theme_post_summary() {
-  wpmp_theme_post();
-  print '</div>';
-}
-
-function wpmp_theme_post($single = false) {
-  global $wpmp_summary_first;
-  if (!isset($wpmp_summary_first)) {
-    $wpmp_summary_first=true;
-  }
-  $summary = get_option('wpmp_theme_post_summary');
-  $metadata = get_option('wpmp_theme_post_summary_metadata')=='true';
-  if ($single || $metadata) {
-    print '<p class="metadata">'. get_the_time('F jS, Y') . ' by ' . get_the_author() . '</p>';
-  }
-  if ($single || ($summary!='none' && ($summary!='firstteaser' || $wpmp_summary_first))) {
-    print '<div class="entry">';
-    the_content(__('Read more', 'wpmp'));
-    print '</div>';
-    $wpmp_summary_first = false;
-  }
-  if ($single || $metadata) {
-    print '<p class="metadata">' . __('Posted in ', 'wpmp');
-    the_category(', ');
-    edit_post_link('Edit', ' | ', '');
-    print ' | ';
-    comments_popup_link('No comments', '1 comment', '% comments');
-    print '</p>';
-  }
-}
 ?>
 
 
     
-    </body>
+</body>
 </html>
