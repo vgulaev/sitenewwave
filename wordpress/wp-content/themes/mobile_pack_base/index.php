b32510a0260d25f87cb5a58795700fbac5e43c24
alexander fedorov <elf607@ya.ru>
2013-04-01 09:54:14 +0600

    working fine now

--- wordpress/wp-content/themes/mobile_pack_base/index.php
+++ wordpress/wp-content/themes/mobile_pack_base/index.php
@@ -1,36 +1,37 @@
+<?php // get_header(); ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-		<title>My prj</title>
-		<link rel="stylesheet" href="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
-		<!--link rel="stylesheet" href="m/my.css" /-->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<!--script src="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script-->
-        <!--script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script-->
-		<script src="lib/frameworks/jquerymobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
-		<script src="m/my.js"></script>
-		<!-- User-generated css -->
-		<style>
-		</style>
-		<!-- User-generated js -->
-		<script>
-			try {
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <title>Тримет новости</title>
+        <link rel="stylesheet" href="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+        <!--link rel="stylesheet" href="m/my.css" /-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <!--script src="https://s3.amazonaws.com/codiqa-cdn/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script-->
+        <script src="http://code.jquery.com/mobile/1.3.0/jquery.mobile-1.3.0.min.js"></script>
+        <!-- // <script src="lib/frameworks/jquerymobile/1.3.0/jquery.mobile-1.3.0.min.js"></script> -->
+        <script src="m/my.js"></script>
+        <!-- User-generated css -->
+        <style>
+        </style>
+        <!-- User-generated js -->
+        <script>
+            try {
 
-				$(function() {
+                $(function() {
 
-				});
+                });
 
-			} catch (error) {
-				console.error("Your javascript has an error: " + error);
-			}
+            } catch (error) {
+                console.error("Your javascript has an error: " + error);
+            }
 
-		</script>
-	</head>
-	<body>
+        </script>
+    </head>
+    <body>
 <!-- Home -->
 <div data-role="page" id="Main">
     <div data-role="content">
@@ -41,117 +42,119 @@
                 </div>
             </div>
             <div class="ui-block-b" style="width: 110px;">
-                <div>	<a href="http://trimet.ru" data-transition="fade"> Полная версия сайта </a>
+                <div>   <a href="http://trimet.ru" data-transition="fade"> Полная версия сайта </a>
 
                 </div>
             </div>
         </div>
         <ul data-role="listview" data-divider-theme="b" data-inset="true">
-            <li data-theme="c">	<a href="#page3" data-transition="slide"> Новости </a>
-
-            </li>
-            <li data-theme="c">	<a href="#page4" data-transition="slide"> Контакты </a>
-
-            </li>
-            <li data-theme="c">	<a href="#assortiment" data-transition="slide"> Ассортимент </a>
-
-            </li>
+            
+            <?php 
+            while (have_posts()) {
+                the_post();
+                // print '<div class="post" id="post-' . get_the_ID() . '">';
+                if(is_single() || is_page()) {
+                    print '<h1>' . get_the_title() . '</h1>';
+                    wpmp_theme_post_single();
+                } else {
+                    print '<li data-theme="c">';
+                    print '<a href="#article'.get_the_ID().'" data-transition="slide">' . get_the_title() . '</a>';
+                    // wpmp_theme_post_summary();
+                    print '</li>';
+              }
+            } 
+            ?>
         </ul>
         <!--select id="sdsfsd" name="select-choice-1">
             <option value="null">Выберите группу товаров</option>
         </select-->
-        <div id="output">	<b> Enter content here... </b>
-
-        </div>
+        
     </div>
 </div>
 <!-- news -->
-<div data-role="page" id="page3">
-    <div class="ui-grid-a">
-        <div class="ui-block-a" style="width: 180px;">
-            <div style="">
-                <img style="width: 160px; height: 47px" src="img/logo.png">
-            </div>
-        </div>
-        <div class="ui-block-b" style="width: 110px;">
-            <div>	<a href="http://trimet.ru" data-transition="fade"> Полная версия сайта </a>
 
-            </div>
-        </div>
-    </div>
-    <div data-role="content">
+
+<?php 
+    while (have_posts()) {
+        the_post();
+        // print '<div class="post" id="post-' . get_the_ID() . '">';
+        print '<div data-role="page" id="article'.get_the_ID().'">';
+        ?>
+
+        <div data-role="content">
+        <?php 
+            print '<h1>' . get_the_title() . '</h1>';
+            print get_the_content(); 
+        ?>
+
         <ul data-role="listview" data-divider-theme="b" data-inset="true">
-            <li data-theme="c" data-icon="arrow-l">	<a href="#Main" data-transition="slide"> Назад </a>
-
-            </li>
-        </ul>
-    </div>
-</div>
-<!-- Contacts -->
-<div data-role="page" id="page4">
-    <div data-role="content">
-        <div class="ui-grid-a">
-            <div class="ui-block-a" style="width: 180px;">
-                <div style="">
-                    <img style="width: 160px; height: 47px" src="img/logo.png">
-                </div>
-            </div>
-            <div class="ui-block-b" style="width: 110px;">
-                <div>	<a href="http://trimet.ru" data-transition="fade"> Полная версия сайта </a>
-
-                </div>
-            </div>
-        </div>
-        <div data-role="collapsible-set">
-            <div data-role="collapsible">
-                	<h3> Телефоны </h3>
-
-                <div>
-                    <p>	<b> Отдел продаж: <a title="Звонить" href="tel:+73452-520-670" data-mce-href="tel:+73452-520-670"> +7-3452-520670
-								<br>
-								<br>
-								</a> </b>
-	<b> Снабжение: <a title="Звонить" href="tel:+73452-520-675" data-mce-href="tel:+73452-520-675"> +7-3452-520675 </a> </b>
-
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div data-role="collapsible-set">
-            <div data-role="collapsible">
-                	<h3> Адреса </h3>
-
-                <div>
-                    <p>	<b> Офис: 625014 г. Тюмень, ул. Республики, 278а, стр.1 </b>
-
-                    </p>
-                </div>
-            </div>
-        </div>
-        <ul data-role="listview" data-divider-theme="b" data-inset="true">
-            <li data-theme="c" data-icon="arrow-l">	<a href="#Main" data-transition="slide"> Назад </a>
-
-            </li>
-        </ul>
-    </div>
-</div>
-<div data-role="page" id="assortiment">
-    <div data-role="content">
-        <ul data-role="listview" data-divider-theme="b" data-inset="true">
-            <li data-theme="c" data-icon="arrow-l">	<a href="#Main" data-transition="slide"> Назад </a>
+            <li data-theme="c" data-icon="arrow-l"> <a href="#Main" data-transition="slide"> Назад </a>
 
             </li>
         </ul>
         <!--a id=" ButtonEx" data-role="button" href="#page1" onclick="doSomething(); return false" rel="external"> Button </a-->
-        <select id="NamingRules" name="select-choice-1">
-            <option value="null">Выберите группу товаров</option>
-        </select>
-		<fieldset data-role="controlgroup">
-		<div id="queryconditionfields"></div>
-		</fieldset>
-        <ul id="nomenklaturalist" data-role="listview" data-divider-theme="b" data-inset="true"></ul>
-		<!--a id=" ButtonEx" data-role="button" href="#page1" onclick="doSomething2(); return false" rel="external"> Button </a-->
+        
+
     </div>
 </div>
-	</body>
+    <?php
+        // print '<li data-theme="c">';
+        // print '<a href="#article'.get_the_ID().'" data-transition="slide">' . get_the_title() . '</a>';
+        // // wpmp_theme_post_summary();
+        // print '</li>';
+  
+} 
+
+
+
+function wpmp_theme_post_single() {
+  wpmp_theme_post(true);
+  print '<p class="metadata">'; previous_post_link(__('Previous post:', 'wpmp') . ' %link'); print '<br />'; next_post_link(__('Next post:', 'wpmp') . ' %link'); print '</p>';
+  if(!function_exists('wpmp_transcoder_is_last_page') || wpmp_transcoder_is_last_page()) {
+    global $post;
+    if (!$post->comment_status=='open') {
+      print '<p class="metadata">' . __('Comments are closed for this post.', 'wpmp') . '</p>';
+      print '</div>';
+    } else {
+      print '</div>';
+      comments_template();
+    }
+  }
+}
+
+function wpmp_theme_post_summary() {
+  wpmp_theme_post();
+  print '</div>';
+}
+
+function wpmp_theme_post($single = false) {
+  global $wpmp_summary_first;
+  if (!isset($wpmp_summary_first)) {
+    $wpmp_summary_first=true;
+  }
+  $summary = get_option('wpmp_theme_post_summary');
+  $metadata = get_option('wpmp_theme_post_summary_metadata')=='true';
+  if ($single || $metadata) {
+    print '<p class="metadata">'. get_the_time('F jS, Y') . ' by ' . get_the_author() . '</p>';
+  }
+  if ($single || ($summary!='none' && ($summary!='firstteaser' || $wpmp_summary_first))) {
+    print '<div class="entry">';
+    the_content(__('Read more', 'wpmp'));
+    print '</div>';
+    $wpmp_summary_first = false;
+  }
+  if ($single || $metadata) {
+    print '<p class="metadata">' . __('Posted in ', 'wpmp');
+    the_category(', ');
+    edit_post_link('Edit', ' | ', '');
+    print ' | ';
+    comments_popup_link('No comments', '1 comment', '% comments');
+    print '</p>';
+  }
+}
+?>
+
+
+    
+    </body>
 </html>
