c30bad69f6b116e8ad3c96a209d5e5cea58ffd38
alexander fedorov <elf607@ya.ru>
2013-04-30 16:34:23 +0600

    divided js into common js code & goods handler js

--- 1cengine/site/index.php
+++ 1cengine/site/index.php
@@ -79,6 +79,7 @@ echo $template_string;
 <script type="text/javascript" src='/lib/frameworks/jqrequired/jquery.blockUI.js'> </script>
 <script type="text/javascript" src='/lib/frameworks/jqrequired/jquery.cookie.js'> </script>
 <script type="text/javascript" src='/1cengine/site/js/modern_uiJs.js'> </script>
+<script type="text/javascript" src='/1cengine/site/js/modern_ui_goods_handler.js'> </script>
 
 
 <link rel="stylesheet" type="text/css" href="/1cengine/site/modern_style.css" />
