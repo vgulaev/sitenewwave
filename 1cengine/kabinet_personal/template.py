e4fecb50d2bd25cd16bd003712fe4d739c089532
alexander fedorov <elf607@ya.ru>
2013-07-31 15:01:18 +0600

    styling

--- 1cengine/kabinet_personal/template.py
+++ 1cengine/kabinet_personal/template.py
@@ -15,6 +15,81 @@ sys.path.append(lib_path)
 _PATH_ = os.path.abspath(os.path.dirname(__file__))
 
 
+def compose_password_part():
+    fieldset_tag = soup.new_tag("fieldset")
+    fieldset_tag["title"] = "Смена пароля"
+    legend_tag = soup.new_tag("legend")
+    legend_tag.append("Сменить пароль")
+    
+    fieldset_tag.append(legend_tag)
+    
+    table_tag = soup.new_tag("table")
+    table_tag["id"] = "passwd_tab"
+
+    #########
+
+    old_passwd_tr = soup.new_tag("tr")
+
+    old_passwd_label_td = soup.new_tag("td")
+    old_passwd_label_td.append("Старый пароль")
+    old_passwd_input_td = soup.new_tag("td")
+    old_passwd_input = soup.new_tag("input")
+    old_passwd_input["type"] = "password"
+    old_passwd_input["id"] = "old_passwd_input"
+    old_passwd_input_td.append(old_passwd_input)
+
+    old_passwd_tr.append(old_passwd_label_td)
+    old_passwd_tr.append(old_passwd_input_td)
+
+    table_tag.append(old_passwd_tr)
+
+    #########
+
+    passwd_tr = soup.new_tag("tr")
+    
+    passwd_label_td = soup.new_tag("td")
+    passwd_label_td.append("Новый пароль")
+    passwd_input_td = soup.new_tag("td")
+    passwd_input = soup.new_tag("input")
+    passwd_input["type"] = "password"
+    passwd_input["id"] = "passwd_input"
+    passwd_input_td.append(passwd_input)
+
+    passwd_tr.append(passwd_label_td)
+    passwd_tr.append(passwd_input_td)
+
+    table_tag.append(passwd_tr)
+
+    ##########
+
+    repeat_passwd_tr = soup.new_tag("tr")
+    
+    repeat_passwd_label_td = soup.new_tag("td")
+    repeat_passwd_label_td.append("Повторите пароль")
+    repeat_passwd_input_td = soup.new_tag("td")
+    repeat_passwd_input = soup.new_tag("input")
+    repeat_passwd_input["type"] = "password"
+    repeat_passwd_input["id"] = "repeat_passwd_input"
+    repeat_passwd_input_td.append(repeat_passwd_input)
+
+    repeat_passwd_tr.append(repeat_passwd_label_td)
+    repeat_passwd_tr.append(repeat_passwd_input_td)
+
+    table_tag.append(repeat_passwd_tr)
+
+    ##########
+    
+    passwd_button_div = soup.new_tag("div")
+    passwd_button_div["id"] = "passwd_button"
+    passwd_button_div.append("Изменить")
+
+    #########
+    
+    fieldset_tag.append(table_tag)
+    fieldset_tag.append(passwd_button_div)
+
+    return "<div>"+str(fieldset_tag)+"</div>"
+
 def show_personal():
 
     python_lib_name = "user"
@@ -33,34 +108,8 @@ def show_personal():
             </div>
         """
     else:
-        
-        table_tag = soup.new_tag("table")
-        table_tag["id"] = "personal_tab"
 
-        passwd_tr = soup.new_tag("tr")
-        
-        passwd_label_td = soup.new_tag("td")
-        passwd_label_td.append("Пароль")
-
-        passwd_input_td = soup.new_tag("td")
-        passwd_input = soup.new_tag("input")
-        passwd_input["type"] = "password"
-        passwd_input["id"] = "passwd_input"
-        passwd_input_td.append(passwd_input)
-
-        passwd_button_td = soup.new_tag("td")
-        passwd_button_div = soup.new_tag("div")
-        passwd_button_div["id"] = "passwd_button"
-        passwd_button_div.append("Изменить")
-        passwd_button_td.append(passwd_button_div)
-
-        passwd_tr.append(passwd_label_td)
-        passwd_tr.append(passwd_input_td)
-        passwd_tr.append(passwd_button_td)
-
-        table_tag.append(passwd_tr)
-
-        return "<div>"+str(table_tag)+"</div>"        
+         return compose_password_part()       
 
 def __main__(funkt):
     return eval(funkt)
