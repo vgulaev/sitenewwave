1c5a6fac4560f2b3ddfc150111e0eff9f2fad230
alexander fedorov <elf607@ya.ru>
2013-08-09 16:56:41 +0600

    current updates of making shipping

---
+++ 1cengine/kabinet_shipping/template.py
@@ -0,0 +1,53 @@
+#!/web/trimetru/python/bin/python2.6
+# -*- coding:utf-8 -*-
+
+import sys, os
+import cgi
+import cgitb; cgitb.enable()
+import imp
+import Cookie
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup()
+
+lib_path = os.path.abspath('1cengine/py_scripts/')
+sys.path.append(lib_path)
+_PATH_ = os.path.abspath(os.path.dirname(__file__))
+
+def get_shipping_list(uid):
+
+    python_lib_name = "get_shipping_list"
+    payment_lib = imp.load_source(python_lib_name, lib_path+"/"+python_lib_name+".py")
+
+    return "<div>"+str(payment_lib.get_shipping_list(uid))+"</div>" 
+
+    return "<div>nya</div>"
+
+def show_payments():
+
+    python_lib_name = "user"
+    user_lib = imp.load_source(python_lib_name, lib_path+"/"+python_lib_name+".py")
+
+    user = user_lib.User()
+
+    if user.check_SID() == False:
+        return """
+            <div>
+            <script type="text/javascript">
+                $(document).ready( function(){
+                        window.location = "/kabinet/authorization/"
+                    })
+            </script>
+            </div>
+        """
+    else:
+
+        cookie = Cookie.SimpleCookie(os.environ["HTTP_COOKIE"])
+        sid = cookie["sid"].value
+        uid_1c = user_lib.__main__("get_1c_sid('"+sid+"')")
+        
+        return get_shipping_list(uid_1c)       
+
+def __main__(funkt):
+    return eval(funkt)
---
+++ 1cengine/py_scripts/get_shipping_list.py
@@ -0,0 +1,134 @@
+#!/web/trimet/python/bin/python2.6
+# -*- coding: utf-8 -*-
+
+import sys, os
+import cgi
+import cgitb; cgitb.enable()
+sys.path.insert(0, os.path.expanduser('~/site/python'))
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+# print ("Content-Type: text/html; charset=utf-8\n")
+
+import json
+from suds.client import Client
+from suds.cache import DocumentCache
+
+import logging
+logging.basicConfig(level=logging.INFO)
+if __debug__:
+    logging.getLogger('suds.client').setLevel(logging.DEBUG)
+else:
+    logging.getLogger('suds.client').setLevel(logging.CRITICAL)
+
+_DEVELOPING_ADDRESS_ = "http://192.168.194.14/fedorov_trimet_ut_copy/ws/"
+# _DEVELOPING_ADDRESS_ = "http://192.168.194.14/DemoTrimet/ws/"
+_PRODUCTION_ADDRESS_ = "http://195.239.221.58:30080/DemoTrimet/ws/"
+
+if "dev" in os.environ["SERVER_NAME"]:
+    _CURRENT_ADDRESS_ = _DEVELOPING_ADDRESS_
+else:
+    _CURRENT_ADDRESS_ = _PRODUCTION_ADDRESS_
+
+
+def get_shipping_list(UID):
+
+    post = {}
+
+    if "POST_DATA" in os.environ:
+        raw_post = os.environ["POST_DATA"]
+    else:
+        raw_post = sys.stdin.read()
+
+    if raw_post != "":
+        pre_post = raw_post.split("&")
+        # print pre_post
+        for variables in pre_post:
+            # print variables
+            key_var = str(variables).split("=")
+            # print key_var
+            post[key_var[0]] = key_var[1]
+
+    date_from = None
+    date_to = None
+
+    if "dateFrom" in post:
+        if post["dateFrom"] != "":
+            date_from_array = post["dateFrom"].split(".")
+            date_from = date_from_array[2]+"-"+date_from_array[1]+"-"+date_from_array[0]
+            # print date_from
+
+    if "dateTo" in post:
+        if post["dateTo"] != "":
+            date_to_array = post["dateTo"].split(".")
+            date_to = date_to_array[2]+"-"+date_to_array[1]+"-"+date_to_array[0]
+
+    client = Client(_CURRENT_ADDRESS_+'privetoffice.1cws?wsdl', location = _CURRENT_ADDRESS_+"privetoffice.1cws")
+    # client = Client('http://192.168.194.14/fedorov_trimet_ut_copy/ws/privetoffice.1cws?wsdl', location = "http://192.168.194.14/fedorov_trimet_ut_copy/ws/privetoffice2.1cws?")
+
+    client.set_options(cache=DocumentCache())
+
+
+    result = client.service.ShippingList(UID,date_from,date_to)
+
+    # return result
+
+    # print "nya"
+    # print result
+
+    listShipping = """
+         
+        <div class="dateChooser">
+            <form method="POST" action="/kabinet/shipping/" id="dateForm">
+                Показать заказы в период: <input type="textarea" name="dateFrom" class="dateInput dateFrom" /> - <input type="textarea" name="dateTo" class="dateInput dateTo" />
+                <div class="datePickButton">Обновить журнал</div>
+            </form>
+        </div>
+    """
+
+    listShipping = listShipping + """
+        <div class="shippingListHeader">
+            <span>Номер</span>
+            <span>Сумма</span>
+            <span>Тоннаж</span>
+            <span><a href="javascript:pass()">Дата<img class="date_arrow" src="/1cengine/kabinet_shipping/arrow_down.svg" /></a></span>
+        </div>
+        <div id="shippingsContainer">
+    """
+
+    odd = "odd"
+
+    shippings = ""
+    for shipping in result[2][0]:
+        shippings = shippings + """
+            <div class="shippingItem """+odd+""" ">
+                <div>
+                    <span class="openShippingDownload">
+                    <img class="ar_img" src="/1cengine/kabinet_shipping/arrow.svg" />
+                    """+str(shipping[3])+"""</span>
+                    <span>"""+str(shipping[2])+"""</span>
+                    <span class="weight">""" +str(shipping[4])+ """</span>
+                    <span class="shippingDate">"""+str(shipping[1].split(" ")[0])+"""</span>           
+                </div>
+
+                <p class="shippingDownload">
+                    Скачать платежное поручение: 
+                    <a href='javascript:openLink(\""""+str(shipping[0])+"""\","xlsx")' title="Скачать поручение в формате xls"> xls </a>
+                    <a href='javascript:openLink(\""""+str(shipping[0])+"""\","pdf")' title="Скачать поручение в формате pdf"> pdf </a>
+                    <a href='javascript:openLink(\""""+str(shipping[0])+"""\","ods")' title="Скачать поручение в формате ods"> ods </a>
+                </p>
+            </div>
+        """
+        
+        if odd=="odd":
+            odd=""
+        else:
+            odd = "odd"
+
+    listShipping = listShipping + shippings + "</div>"
+
+    return listShipping
+
+
+def __main__(funct_name):
+    return eval(funct_name)
