9663d8110980f1bce1f122cde43b7c31872bb9d9
alexander fedorov <elf607@ya.ru>
2014-08-19 15:30:41 +0600

    cheeeeeeeeck

--- 1cengine/py_scripts/get_items_bs.py
+++ 1cengine/py_scripts/get_items_bs.py
@@ -287,7 +287,7 @@ class Item():
                         price_array.index(price))
                     span_tag = soup.new_tag("span")
 
-                    price_repr = ( locale.format("%d", float(price), grouping=True) + locale.format("%.2f", float(price))[-3:] ).replace(" ", "\u00a0")
+                    price_repr = ( locale.format("%d", float(price), grouping=True) + locale.format("%.2f", float(price))[-3:] ).replace(" ", "\xc2\xa0")
 
                     span_tag.append(price_repr)
 
@@ -303,7 +303,7 @@ class Item():
 
                     span_tag = soup.new_tag("span")
                     span_tag["itemprop"] = "price"
-                    price_repr = ( locale.format("%d", float(price), grouping=True) + locale.format("%.2f", float(price))[-3:] ).replace(" ", "\u00a0")
+                    price_repr = ( locale.format("%d", float(price), grouping=True) + locale.format("%.2f", float(price))[-3:] ).replace(" ", "\xc2\xa0")
 
                     span_tag.append(price_repr)
                     price_item_tag.append(span_tag)
