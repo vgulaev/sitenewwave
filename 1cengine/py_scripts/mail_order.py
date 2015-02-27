67934a9aa5d52f066a85fb0c1c7e87ef7b6bcdac
alexander fedorov <elf607@ya.ru>
2015-02-27 14:11:56 +0500

    importing libs

--- 1cengine/py_scripts/mail_order.py
+++ 1cengine/py_scripts/mail_order.py
@@ -11,6 +11,7 @@ from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
 
 import cgitb
 cgitb.enable()
