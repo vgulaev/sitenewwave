b9137349252182702f44e4132a89f60775310ca4
alexander fedorov <elf607@ya.ru>
2015-05-27 09:15:05 +0500

    qf

--- sitemap.py
+++ sitemap.py
@@ -23,61 +23,8 @@ else:
 print("<!DOCTYPE html>")
 
 
-def makecontent(path=None):
-    # print path
-    soup = BeautifulSoup(open("locate/ru/templates/mainpage_template.html"))
-    if (debugmode is True):
-        soup.html.noscript.extract()
-        nodes = soup.html.body("script")
-        for currentelement in nodes:
-            currentelement.extract()
-    soupForImport = BeautifulSoup("""<html><div id="main" \
-        style="width: 972px;margin: 0 auto;"><table><thead>
-<th>№</th>
-<th>Link</th>
-<th>Changefreq</th>
-</thead>
-<tbody>
-</tbody>
-</table></div></html>""")
-    soupSiteMap = BeautifulSoup(open("sitemap.xml"), "xml")
-    nodes = soupSiteMap.find_all("url")
-    counter = 0
-    tbody = soupForImport.html.table.tbody
-    for el in nodes:
-        counter = counter + 1
-        tag_tr = soupForImport.new_tag("tr")
-        tag_td = soupForImport.new_tag("td")
-        tag_td.append(str(counter))
-        tag_tr.append(tag_td)
-        tag_td = soupForImport.new_tag("td")
-        tag_a = soupForImport.new_tag("a", href=el.loc.string)
-        tag_a.append(str(el.loc.string))
-        tag_td.append(tag_a)
-        tag_tr.append(tag_td)
-        tag_td = soupForImport.new_tag("td")
-        tag_changefreq = el.find("changefreq")
-        changefreq = "None"
-        if tag_changefreq is not None:
-            changefreq = el.changefreq.string
-        tag_td.append(changefreq)
-        tag_tr.append(tag_td)
-        tbody.append(tag_tr)
 
-    # table = soupForImport.new_tag("table")
-
-    soup.html.body.append(soupForImport.html.div)
-    # soupForImport.append()
-    soupFooter = BeautifulSoup(
-        open("locate/ru/templates/mainfooter_template.html"))
-    node = soupFooter.find("footer", {"id": "footer"})
-    soup.html.body.append(node)
-    # print(soupForImport.prettify("utf-8"))
-    print(soup.prettify("utf-8"))
-
-# makecontent()
-
-print '''<div class="mod-sitemap">
+sm = '''<div class="mod-sitemap">
     <div>
         <h2><a href="/about/" title="О компании">О компании</a></h2>
         <ul>
@@ -199,3 +146,49 @@ print '''<div class="mod-sitemap">
     <div class="clear">&nbsp;</div>
 </div>
 </div>'''
+
+def makecontent(path=None):
+    # print path
+    soup = BeautifulSoup(open("locate/ru/templates/mainpage_template.html"))
+    if (debugmode is True):
+        soup.html.noscript.extract()
+        nodes = soup.html.body("script")
+        for currentelement in nodes:
+            currentelement.extract()
+    soupForImport = BeautifulSoup(sm)
+    # soupSiteMap = BeautifulSoup(open("sitemap.xml"), "xml")
+    # nodes = soupSiteMap.find_all("url")
+    # counter = 0
+    # tbody = soupForImport.html.table.tbody
+    # for el in nodes:
+    #     counter = counter + 1
+    #     tag_tr = soupForImport.new_tag("tr")
+    #     tag_td = soupForImport.new_tag("td")
+    #     tag_td.append(str(counter))
+    #     tag_tr.append(tag_td)
+    #     tag_td = soupForImport.new_tag("td")
+    #     tag_a = soupForImport.new_tag("a", href=el.loc.string)
+    #     tag_a.append(str(el.loc.string))
+    #     tag_td.append(tag_a)
+    #     tag_tr.append(tag_td)
+    #     tag_td = soupForImport.new_tag("td")
+    #     tag_changefreq = el.find("changefreq")
+    #     changefreq = "None"
+    #     if tag_changefreq is not None:
+    #         changefreq = el.changefreq.string
+    #     tag_td.append(changefreq)
+    #     tag_tr.append(tag_td)
+    #     tbody.append(tag_tr)
+
+    # table = soupForImport.new_tag("table")
+
+    soup.html.body.append(soupForImport.html)
+    # soupForImport.append()
+    soupFooter = BeautifulSoup(
+        open("locate/ru/templates/mainfooter_template.html"))
+    node = soupFooter.find("footer", {"id": "footer"})
+    soup.html.body.append(node)
+    # print(soupForImport.prettify("utf-8"))
+    print(soup.prettify("utf-8"))
+
+makecontent()
