97fbc81e6b21e52dc36b2c7ba31d110fabdcf2bc
alexander fedorov <elf607@ya.ru>
2015-06-25 15:49:20 +0500

    getting done

--- 1cengine/kabinet_shipping/index.js
+++ 1cengine/kabinet_shipping/index.js
@@ -152,3 +152,52 @@ function openLink(linkUID, type) {
     });
 }
 
+// функция отправки сообщения техподдержке
+
+function send_support(){
+    $.blockUI.defaults.css.borderRadius = '10px'; //убираем серую границу
+    $.blockUI.defaults.fadeIn = 100; //ускоряем появление
+    $.blockUI.defaults.fadeOut = 100; //и исчезновение
+    //$.blockUI.defaults.css.left = '39%'; //окно будет в центре
+    $.blockUI.defaults.css.backgroundColor = 'white'
+    $.blockUI.defaults.css.cursor = 'defaults'
+    $.blockUI.defaults.css.boxShadow = '0px 0px 5px 5px rgb(207, 207, 207)'
+    $.blockUI.defaults.css.fontSize = '14px'
+    $.blockUI.defaults.css.width = '450px'
+    $.blockUI.defaults.css.height = '220px'
+    $.blockUI.defaults.css.border = 'none'
+
+    $.blockUI({
+        message: "\
+        <span class='close_button'>x</span>\
+        <div>\
+            <textarea class='ss_textarea' placeholder='Введите ваше сообщение здесь'></textarea>\
+            <span class='send_support_button'>Отправить сообщение в тех.поддержку</span>\
+        </div>"
+    });
+
+    $(".send_support_button").click( function(){
+        $.ajax({
+            type: "POST",
+            url: "/1cengine/py_scripts/send_support.py",
+            async: true,
+            data: "text=" + $(".ss_textarea").val()+"&href="+window.location.pathname,
+            success: function(html){
+                console.log(html)
+                $.unblockUI();
+            }
+        })
+    })
+
+    $(document).on('keyup', function(e) {
+        e.preventDefault();
+        if (e.which === 27) {
+            $.unblockUI();
+        }
+    });
+
+    $(".close_button").click(function() {
+        $.unblockUI();
+    });
+
+}
